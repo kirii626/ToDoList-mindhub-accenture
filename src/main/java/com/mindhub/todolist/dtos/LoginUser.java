db5bab77da07f32b4c08dac36a66e9bf23cefd4f
Alejandra Kiara Justiniano Olmos <kiaraalejandrajustinianoolmos@gmail.com>
2025-01-19 14:52:06 -0300

    Add @NotBlank annotation to input fields to enforce validation

--- src/main/java/com/mindhub/todolist/dtos/LoginUser.java
+++ src/main/java/com/mindhub/todolist/dtos/LoginUser.java
@@ -1,4 +1,12 @@
 package com.mindhub.todolist.dtos;
 
-public record LoginUser(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginUser(@NotBlank(message = "Email is required")
+                        @Email(message = "Invalid email format")
+                        String email,
+
+                        @NotBlank(message = "Password is required")
+                        String password) {
 }
