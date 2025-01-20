6bec497aa69a6778ad926f36279dfe647c1f0b09
Alejandra Kiara Justiniano Olmos <kiaraalejandrajustinianoolmos@gmail.com>
2025-01-20 16:32:15 -0300

    Improve handling of error

--- src/main/java/com/mindhub/todolist/services/implementations/AuthServiceImpl.java
+++ src/main/java/com/mindhub/todolist/services/implementations/AuthServiceImpl.java
@@ -3,10 +3,12 @@ package com.mindhub.todolist.services.implementations;
 import com.mindhub.todolist.config.JwtUtils;
 import com.mindhub.todolist.dtos.NewUserDto;
 import com.mindhub.todolist.dtos.UserDto;
+import com.mindhub.todolist.exceptions.InvalidCredentialsExc;
 import com.mindhub.todolist.services.AuthService;
 import com.mindhub.todolist.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,21 +28,26 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String authenticateAndGenerateToken(String email, String password) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password)
+            );
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String username = authentication.getName();
+            String username = authentication.getName();
 
-        String role = authentication.getAuthorities()
-                .stream()
-                .findFirst()
-                .map(authority -> authority.getAuthority())
-                .orElse("USER");  // Valor predeterminado si no hay roles
+            String role = authentication.getAuthorities()
+                    .stream()
+                    .findFirst()
+                    .map(authority -> authority.getAuthority())
+                    .orElse("USER");  // Valor predeterminado si no hay roles
 
-        return jwtUtil.generateToken(username);
+            return jwtUtil.generateToken(username);
+        } catch (BadCredentialsException e) {
+            throw new InvalidCredentialsExc("Invalid email or password");
+        }
+
     }
 
     @Override
