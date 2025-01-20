7e1aa69859d115a2e3517b949fa0c2cccd95d432
Alejandra Kiara Justiniano Olmos <kiaraalejandrajustinianoolmos@gmail.com>
2025-01-20 16:31:41 -0300

    Improve validations of fields with @Size and @Email

--- src/main/java/com/mindhub/todolist/dtos/NewUserDto.java
+++ src/main/java/com/mindhub/todolist/dtos/NewUserDto.java
@@ -1,6 +1,7 @@
 package com.mindhub.todolist.dtos;
 
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -11,9 +12,11 @@ public class NewUserDto {
     private String username;
 
     @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
 
     public NewUserDto() {
